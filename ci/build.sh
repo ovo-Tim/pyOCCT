f1eb965e650ae48426e1b5a781893cff3a0e91de
ovo-Tim <ovo-tim@qq.com>
2024-01-30 17:15:30 +0800

    Fix ninja [only mac]

--- ci/build.sh
+++ ci/build.sh
@@ -26,7 +26,7 @@ cmake -G "Ninja" \
 if [[ ${HOST} =~ .*linux.* ]]; then
   ninja -j${CPU_COUNT} install
 else
-  cmake --build . -j $(nproc) --target install
+  cmake --build . -j ${CPU_COUNT} --target install
 fi
 
 cd ..
