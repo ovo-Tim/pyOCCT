c436c3befbdb491cc09be02832fc67d6bb63a39a
frmdstryr <frmdstryr@protonmail.com>
2024-02-01 09:54:56 -0500

    ci: Include python in build reqs

--- ci/build.sh
+++ ci/build.sh
@@ -18,7 +18,7 @@ cmake -G "Ninja" \
   -DCMAKE_SYSTEM_PREFIX_PATH="$PREFIX" \
   "${CMAKE_PLATFORM_FLAGS[@]}" \
   -DCMAKE_BUILD_TYPE="Release" \
-  -DPython_ROOT_DIR="$BUILD_PREFIX" \
+  -DPython_ROOT_DIR="$PREFIX" \
   -DPYBIND11_PYTHON_VERSION="$PY_VER" \
   ..
 
